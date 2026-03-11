87eb316153bcdd3fe6c3405743f698e32ebebd07
Alejandro villegas florez <villegasfloreza@gmail.com>
2026-03-10 20:48:23 -0500

    Add Java container organization console app

---
+++ Contenedor.java
@@ -0,0 +1,20 @@
+public class Contenedor {
+    private final String id;
+    private final double peso;
+    private final String origen;
+
+    public Contenedor(String id, double peso, String origen) {
+        this.id = id;
+        this.peso = peso;
+        this.origen = origen;
+    }
+
+    public String getId() { return id; }
+    public double getPeso() { return peso; }
+    public String getOrigen() { return origen; }
+
+    @Override
+    public String toString() {
+        return "Contenedor{id='" + id + "', peso=" + peso + ", origen='" + origen + "'}";
+    }
+}
